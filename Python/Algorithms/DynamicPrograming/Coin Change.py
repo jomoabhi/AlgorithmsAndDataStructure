47ca5242112d840a89987b61c17373901a811a28
jomoabhi <55912822+jomoabhi@users.noreply.github.com>
2020-10-01 21:16:33 +0530

    Update Coin Change.py

--- Python/Algorithms/DynamicPrograming/Coin Change.py
+++ Python/Algorithms/DynamicPrograming/Coin Change.py
@@ -5,9 +5,9 @@
 class Solution:
     #function to compute the number of combinations of different coins that make up that amount 
     def change(self, s: int, l: List[int]) -> int:
-        dp=[0]*(s+1) #creating a list of size(s+1) with initial values as '0'
-        dp[0]=1
+        dp= [0] * (s+1)  #creating a list of size(s+1) with initial values as '0'
+        dp[0] = 1
         for i in l:
-            for j in range(i,s+1):
-                dp[j]+=dp[j-i]   
-        return dp[-1] #returning the output
+            for j in range(i,s + 1):
+                dp[j] + =dp[j - i]   
+        return dp[-1]  #returning the output
